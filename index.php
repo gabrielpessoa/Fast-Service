2a568d3c0638fdc502aa404625aeaeda841f5685
Alessandro018 <alessandrosilva325@gmail.com>
2019-03-17 21:18:12 -0300

    Banco de dados

--- index.php
+++ index.php
@@ -22,14 +22,26 @@
 			
 		</nav>
 
-		<section class="sec1">
-		</section>
-		<section class="conteudo">
+		<div class="header-bg">
+			<h2><label>F</label>ast</h2><br>
+			<h3>Service</h3>
+		</div>
+
+			<section>
+			<h2>Fast-Service</h2>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PcageMaker including versions of Lorem Ipsum.</p>
 		</section>
 	</div>
 
 	<footer class="rodape">©Copyright 2019</footer>
+	<script>
+		$(document).ready(function(){
+			$(window).scroll(function(){
+				$('.header-bg').css('opacity', 1 -
+					$(window).scrollTop()/700)
+			})
+		})
+	</script>
 </body>
 </html>
