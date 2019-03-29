3da40e35d48674cab7db0a1d7cc75ea735df755c
Gabriel Pessoa <gabrielpessoanascimento@gmail.com>
2019-03-29 10:02:25 -0300

    arquivo ajuda.php nao foi

---
+++ php/ajuda.php
@@ -0,0 +1,85 @@
+<?php include("functions.php"); ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Fast-Service</title>
+	
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="../demo-files/demo.css">
+	<script src="../js/jquery.js"></script>
+	<script src="../js/functions.js"></script>
+	<link rel="shortcut icon" type="image/x-png" href="img/3.png">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+</head>
+<body>
+	<div>
+		<nav>
+			
+			<a href="index.php"><img src="../img/3.png"></a>
+			<ul>
+				<li><a href="/">Início</a></li>
+				<li><a href="sobre.php">Sobre</a></li>
+				<li><a href="ajuda.php">Ajuda</a></li>
+				<?php if (isLogged() ){ ?>
+					<li><a href="perfil.php">Minha conta</a></li>
+					<li><a href="servico.php">Anunciar</a></li>
+					<li><a href="logout.php" class="btn-login">Sair</a></li>
+				<?php } else{ ?>
+				<li><a href="register.php">Registrar-se</a></li>
+				<li><a href="#janela" rel="modal" class="btn-login">Login</a></li>
+			<?php } ?>
+			</ul>
+			
+		</nav>
+	</div>
+
+	<br>
+	<center>
+		<div class="header-bg">
+<!-- 		<h2><label>F</label>ast -</h2><br>
+		<h3><label>S</label>ervice</h3> -->
+		</div>
+			<div class="ajuda">
+				<h1>Como faço para me cadastrar?</h1><br> 
+ 				<ul class="ul">
+		 		
+		 		  <li>Na página inicial clique em registrar-se;</li>
+		 			<li>Digite seu nome;</li>
+		 			<li>Nome do usuário;</li>
+		 			<li>E-mail;</li>
+		 			<li>Sexo (Masculino ou feminino);</li>
+		 			<li>Selecionar tipo de conta (Anuciante ou Cliente);</li>
+		 			<li>Criar senha;</li>
+		 			<li>Confirmar senha;</li>
+ 					
+ 				</ul>  
+			</div>
+	</center>
+
+	<div class="window" id="janela">
+		<center>
+			<a href="#" class="fechar">X</a>
+			<h4>Login</h4>
+			<hr>
+			<form action="login2.php" method="POST">
+				<p>Usuário</p><br>
+				<input type="text" name="username" placeholder="Digite aqui"><br>
+				<p>Senha</p><br>
+				<input type="password" name="password" placeholder="Digite aqui"><br>
+				<button type="submit">Entrar</button><br>
+				<a href="#">Esqueceu sua senha?</a>
+			</form>
+		</center>
+	</div>
+
+	<div id="mascara">
+		
+	</div>
+
+	<footer class="rodape">©Copyright 2019</footer>
+
+</body>
+</html>
