1c4a847d43439b676a5cbfaba296bf05af26f5b0
Alessandro018 <alessandrosilva325@gmail.com>
2019-07-07 02:59:35 -0300

    Organização

--- php/view/ajuda_view.php
+++ php/view/ajuda_view.php
@@ -39,58 +39,58 @@
 			 		    <div class="duvida-cadastro-user">
 				 		  	<p><h3>Passo 1</h3></p>
 				 		  	<p>Navege até o menu e clique em "REGISTRAR-SE"</p>
-				 		  	<p><img src="../../img/cadastro1.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/cadastro1.png"></p>
 				 		  	<p><h3>Passo 2</h3></p>
 				 		  	<p>Preencha corretamente todos os campos do formulário.<br> OBS :  o campo "Usuário" será usado para fazer login e a "Senha" deve conter no mínimo 6 caracteres.</p>
-				 		  	<p><img src="../../img/cadastro1.2.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/cadastro1.2.png"></p>
 				 		  	<p><h3>Passo 3</h3></p>
 				 		  	<p>Clique no botão "Cadastrar"</p>
-				 		  	<p><img src="../../img/cadastro1.3.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/cadastro1.3.png"></p>
 			 		    </div>
 			 			<li><a href="#" id="duvi-ads">Como cadastrar um anúncio?</a></li>
 			 			<div class="duvida-cadastro-ads">
 				 		  	<p><h3>Passo 1</h3></p>
 				 		  	<p>Navege até o menu e clique em "Anunciar"</p>
-				 		  	<p><img src="../../img/cadastro3.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/cadastro3.png"></p>
 				 		  	<p><h3>Passo 2</h3></p>
 				 		  	<p>Insira o título do seu anúncio</p>
-				 			<p><img src="../../img/cadastro4.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro4.png"></p>
 				 			<p><h3>Passo 3</h3></p>
 				 			<p>Selecione a categoria no qual se encaixa o seu anúncio</p>
-				 			<p><img src="../../img/cadastro5.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro5.png"></p>
 				 			<p><h3>Passo 4</h3></p>
 				 			<p>Selecione a subcategoria no qual se encaixa o seu anúncio</p>
-				 			<p><img src="../../img/cadastro6.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro6.png"></p>
 				 			<p><h3>Passo 5</h3></p>
 				 			<p>Se for de sua preferência, descreva sobre o que você está anunciando (255 é o máximo de caracteres permitidos</p>
-				 			<p><img src="../../img/cadastro7.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro7.png"></p>
 				 			<p><h3>Passo 6</h3></p>
 				 			<p>Cadastre a localização. Para isso basta preencher corretamente o CEP e o Número do estabelecimento, os demais campos serão preenchidos automaticamente</p>
-				 			<p><img src="../../img/cadastro8.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro8.png"></p>
 				 			<p><h3>Passo 7</h3></p>
 				 			<p>Digite o preço</p>
-				 			<p><img src="../../img/cadastro9.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro9.png"></p>
 				 			<p><h3>Passo 8</h3></p>
 				 			<p>Adicionar fotos (opcional)</p>
-				 			<p><img src="../../img/cadastro10.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro10.png"></p>
 				 			<p><h3>Passo 9</h3></p>
 				 			<p>Clique no botão "Cadastrar" e pronto</p>
-				 			<p><img src="../../img/cadastro11.png"></p>
+				 			<p><img src="../../img/prints_ajuda/cadastro11.png"></p>
 			 		    </div>
 			 			<li><a href="#" id="duvi-dados">Como posso alterar meus dados?</a></li>
 			 			<div class="duvida-alterar-dados">
 				 		  	<p><h3>Passo 1</h3></p>
 			 				<p>Navegue até o menu e clique no link "Minha conta"</p>
-				 		  	<p><img src="../../img/anuncios.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/anuncios.png"></p>
 				 		  	<p><h3>Passo 2</h3></p>
 			 				<p>Navegue até a nova janela que abriu e clique no link "Meu perfil"</p>
-				 		  	<p><img src="../../img/alterar-dados.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/alterar-dados.png"></p>
 				 		  	<p><h3>Passo 3</h3></p>
 			 				<p>Clique no link "Alterar meus dados"</p>
-				 		  	<p><img src="../../img/alterar-dados2.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/alterar-dados2.png"></p>
 				 		  	<p><h3>Passo 4</h3></p>
 			 				<p>Faça as alterações que desejar e por fim, clique no botão de "Alterar dados"</p>
-				 		  	<p><img src="../../img/alterar-dados3.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/alterar-dados3.png"></p>
 			 		    </div>
 			 		    <!-- <li><a href="#" id="duvi-login">Como faço login?</a></li>
 			 		    <div class="duvida-login">
@@ -101,10 +101,10 @@
 			 		    <div class="duvida-my-ads">
 			 		  		<p><h3>Passo 1</h3></p>
 			 				<p>Navegue até o menu e clique no link "Minha conta"</p>
-				 		  	<p><img src="../../img/anuncios.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/anuncios.png"></p>
 				 		  	<p><h3>Passo 2</h3></p>
 			 				<p>Navegue até a nova janela que abriu e clique no link "Meus anúncios"</p>
-				 		  	<p><img src="../../img/anuncios2.png"></p>
+				 		  	<p><img src="../../img/prints_ajuda/anuncios2.png"></p>
 			 		    </div>
 			 		<br>
  				</ul>  
